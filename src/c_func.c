3d0a75e3de7e6a132201b528ec1bcbff67d621f8
lucascsd <lucas.zalazar6@gmail.com>
2021-09-25 15:52:45 -0300

    Se sube archivos de ejercicio a repositorio. Ejercicio de producto escalar en este commit

---
+++ src/c_func.c
@@ -0,0 +1,31 @@
+#include "c_func.h"
+
+/* Funcion que realiza la suma de 32 bits */
+uint32_t c_sum (uint32_t firstOperand, uint32_t secondOperand)
+{
+	return (firstOperand + secondOperand);
+}
+
+/* Funcion que realiza la suma de 64 bits */
+uint64_t c_sum64 (uint64_t firstOperand64, uint64_t secondOperand64)
+{
+	return (firstOperand64 + secondOperand64);
+}
+
+/* Funcion que pone todos los elementos de un arreglo a 0 */
+void zeros( uint32_t *vector, uint32_t longitud)
+{
+	for (uint32_t i = 0; i < longitud; i++)
+	{
+		vector[i] = 0;
+	}
+}
+
+/* Funcion que realiza el producto escalar */
+void c_productoEscalar32( uint32_t *vectorIn, uint32_t *vectorOut, uint32_t longitud, uint32_t escalar)
+{
+
+	for ( uint8_t i = 0; i < longitud; i++)
+		vectorOut[i] = escalar * vectorIn[i];
+
+}
